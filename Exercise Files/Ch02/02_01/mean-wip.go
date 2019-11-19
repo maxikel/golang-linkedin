f7874a6d751095da6f8fc5ec1822b65750d139f0
Adam Kelly <adam.kelly@guardian.co.uk>
2019-11-19 09:18:38 +0000

    Post completion of ex0201

--- Exercise Files/Ch02/02_01/mean-wip.go
+++ Exercise Files/Ch02/02_01/mean-wip.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	var x int
-	var y int
+	var x float64
+	var y float64
 
 	x = 1
 	y = 2
@@ -15,7 +15,7 @@ func main() {
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean int
-	mean = (x + y) / 2
+	var mean float64
+	mean = (x + y) / 2.0
 	fmt.Printf("result: %v, type of %T\n", mean, mean)
 }
